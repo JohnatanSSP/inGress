f18b077c714ac497434ffdb428ae1883e7cf75cd
Johnatan S S Paixão <johnatan.sousa@gmail.com>
2025-10-08 17:45:31 -0300

    Feat: fist commit

---
+++ src/test/java/johnatanSSP/inGress/InGressApplicationTests.java
@@ -0,0 +1,13 @@
+package johnatanSSP.inGress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InGressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
