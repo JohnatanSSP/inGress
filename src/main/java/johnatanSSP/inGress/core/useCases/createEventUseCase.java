583ed0df311d00dfbd35a7db7f2bab8b4b00ef8f
Johnatan S S Paixão <johnatan.sousa@gmail.com>
2025-10-14 01:40:54 -0300

    feat: create event usecase

---
+++ src/main/java/johnatanSSP/inGress/core/useCases/createEventUseCase.java
@@ -0,0 +1,8 @@
+package johnatanSSP.inGress.core.useCases;
+
+import johnatanSSP.inGress.core.entities.Ticket;
+
+public interface createEventUseCase {
+
+    public Ticket execute(Ticket ticket);
+}
---
+++ src/main/java/johnatanSSP/inGress/core/useCases/createEventUseCaseImpl.java
@@ -0,0 +1,12 @@
+package johnatanSSP.inGress.core.useCases;
+
+import johnatanSSP.inGress.core.entities.Ticket;
+
+public class createEventUseCaseImpl implements createEventUseCase {
+
+    @Override
+    public Ticket execute(Ticket ticket) {
+        return ticket;
+    }
+
+}
