3f19e5665a37413ed3f5bf3e3a229c4456298643
Johnatan S S Paixão <johnatan.sousa@gmail.com>
2025-10-14 00:42:45 -0300

    feat: entity

--- src/main/java/johnatanSSP/inGress/core/entities/Ticket.java
+++ src/main/java/johnatanSSP/inGress/core/entities/Ticket.java
@@ -1,5 +1,32 @@
 package johnatanSSP.inGress.core.entities;
 
-public record Ticket() {
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+public record Ticket(
+
+        Long id,
+        String title,
+        String description,
+        String location,
+        String creator,
+        String category,
+        LocalDateTime creationDate,
+        LocalDateTime updateDate,
+        String url,
+        String image,
+        @Enumerated
+        StatusTicket status,
+        @Enumerated
+        RatedEvent rated
+) {
+
+    public enum StatusTicket{
+        OPEN, IN_PROGRESS, CLOSED, CANCELED
+    }
+
+    public enum RatedEvent {
+        BAD, AVERAGE, GOOD, VERY_GOOD, SENSATIONAL
+    }
 
 }
