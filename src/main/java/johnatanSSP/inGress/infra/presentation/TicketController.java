59c5254d601fd774ffe393ef70a48321f19d9009
Johnatan S S Paixão <johnatan.sousa@gmail.com>
2025-10-15 18:26:27 -0300

    refactor: controller and implements DTO

--- src/main/java/johnatanSSP/inGress/infra/presentation/TicketController.java
+++ src/main/java/johnatanSSP/inGress/infra/presentation/TicketController.java
@@ -1,18 +1,31 @@
 package johnatanSSP.inGress.infra.presentation;
 
 import johnatanSSP.inGress.core.entities.Ticket;
+import johnatanSSP.inGress.core.useCases.CreateEventUseCase;
 import johnatanSSP.inGress.infra.dtos.TicketDto;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import johnatanSSP.inGress.infra.mapper.TicketDtoMapper;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/")
 public class TicketController {
 
+    private final CreateEventUseCase createEventUseCase;
+    private final TicketDtoMapper ticketDtoMapper;
+
+    public TicketController(CreateEventUseCase createEventUseCase, TicketDtoMapper ticketDtoMapper) {
+        this.createEventUseCase = createEventUseCase;
+        this.ticketDtoMapper = ticketDtoMapper;
+    }
+
     @PostMapping("createTicket")
-    public String createTicket(@RequestBody TicketDto ticket){
-        return "ticket";
+    public TicketDto createTicket(@RequestBody TicketDto Dto){
+        Ticket ticket = createEventUseCase.execute(ticketDtoMapper.toDomain(Dto));
+        return ticketDtoMapper.toDto(ticket);
+    };
+
+    @GetMapping
+    public String ListTickets(){
+        return "listar";
     }
 }
