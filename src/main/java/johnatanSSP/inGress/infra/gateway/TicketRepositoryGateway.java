2535c28e1435d47775cc2ffa2a451d5653521b26
Johnatan S S Paixão <johnatan.sousa@gmail.com>
2025-10-16 00:22:47 -0300

    Update TicketRepositoryGateway.java

--- src/main/java/johnatanSSP/inGress/infra/gateway/TicketRepositoryGateway.java
+++ src/main/java/johnatanSSP/inGress/infra/gateway/TicketRepositoryGateway.java
@@ -1,6 +1,19 @@
 package johnatanSSP.inGress.infra.gateway;
 
+import johnatanSSP.inGress.core.entities.Ticket;
 import johnatanSSP.inGress.core.gateway.TicketGateway;
+import johnatanSSP.inGress.infra.persistence.TicketRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 
+@Component
+@RequiredArgsConstructor
 public class TicketRepositoryGateway implements TicketGateway {
+
+    private final TicketRepository repository;
+
+    @Override
+    public Ticket CreateTicket(Ticket ticket) {
+        return repository.save(ticket);
+    }
 }
