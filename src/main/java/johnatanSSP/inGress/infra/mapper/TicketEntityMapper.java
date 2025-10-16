fed78dc1376aa3d9b297cef48479186e21f44a2e
Johnatan S S Paixão <johnatan.sousa@gmail.com>
2025-10-16 00:22:39 -0300

    Create TicketEntityMapper.java

---
+++ src/main/java/johnatanSSP/inGress/infra/mapper/TicketEntityMapper.java
@@ -0,0 +1,48 @@
+package johnatanSSP.inGress.infra.mapper;
+
+import johnatanSSP.inGress.core.entities.Ticket;
+import johnatanSSP.inGress.infra.persistence.TicketEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TicketEntityMapper {
+
+    public TicketEntity toEntity(Ticket ticket){
+        return new TicketEntity(
+                ticket.id(),
+                ticket.description(),
+                ticket.location(),
+                ticket.title(),
+                ticket.creator(),
+                ticket.category(),
+                ticket.startDate(),
+                ticket.endDate(),
+                ticket.organization(),
+                ticket.url(),
+                ticket.identify(),
+                ticket.image(),
+                ticket.status(),
+                ticket.rated()
+        );
+    }
+
+    public Ticket toDomain(TicketEntity Entity){
+        return new Ticket(
+                Entity.getId(),
+                Entity.getTitle(),
+                Entity.getDescription(),
+                Entity.getLocation(),
+                Entity.getCreator(),
+                Entity.getCategory(),
+                Entity.getStartDate(),
+                Entity.getEndDate(),
+                Entity.getOrganization(),
+                Entity.getUrl(),
+                Entity.getIdentify(),
+                Entity.getImage(),
+                Entity.getStatus(),
+                Entity.getRated()
+        );
+    }
+
+}
