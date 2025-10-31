20141c18d043b6f98f844fa0aa6b6ecdae21d9db
Johnatan S S Paixão <johnatan.sousa@gmail.com>
2025-10-31 00:41:17 -0300

    feat: configuration new bean

--- src/main/java/johnatanSSP/inGress/infra/beans/beanConfiguration.java
+++ src/main/java/johnatanSSP/inGress/infra/beans/beanConfiguration.java
@@ -3,6 +3,8 @@ package johnatanSSP.inGress.infra.beans;
 import johnatanSSP.inGress.core.gateway.TicketGateway;
 import johnatanSSP.inGress.core.useCases.CreateEventUseCase;
 import johnatanSSP.inGress.core.useCases.CreateEventUseCaseImpl;
+import johnatanSSP.inGress.core.useCases.SearchEventUseCase;
+import johnatanSSP.inGress.core.useCases.SearchEventUseCaseImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,4 +15,8 @@ public class beanConfiguration {
     public CreateEventUseCase createEvent(TicketGateway Gateway) {
         return new CreateEventUseCaseImpl(Gateway);
     }
+
+    @Bean
+    public SearchEventUseCase SearchTicket(TicketGateway Gateway) {return new SearchEventUseCaseImpl(Gateway);}
+
 }
