9af4133afef5effa607e86e10f3f3ea3a9fed7b1
Johnatan S S Paixão <johnatan.sousa@gmail.com>
2025-11-11 17:56:33 -0300

    feat: using UUID to generate identification

--- src/main/java/johnatanSSP/inGress/core/useCases/CreateEventUseCaseImpl.java
+++ src/main/java/johnatanSSP/inGress/core/useCases/CreateEventUseCaseImpl.java
@@ -3,6 +3,9 @@ package johnatanSSP.inGress.core.useCases;
 import johnatanSSP.inGress.core.entities.Ticket;
 import johnatanSSP.inGress.core.gateway.TicketGateway;
 import johnatanSSP.inGress.infra.exception.DuplicateEventException;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.UUID;
 
 
@@ -21,7 +24,9 @@ public class CreateEventUseCaseImpl implements CreateEventUseCase {
         if(Gateway.identifierExists(ticket.identify())){
             throw new DuplicateEventException("o identificador "+ ticket.identify()+ "ja existe no DB");
         }
-        String generatedIdentify = UUID.randomUUID().toString();
+        String generatedIdentify = ticket.title().toUpperCase().replace(" ","") +
+                LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss")) +
+                UUID.randomUUID().toString().toUpperCase().replace("-","").substring(0, 10);
 
         Ticket newTicket = new Ticket(
                 null,
